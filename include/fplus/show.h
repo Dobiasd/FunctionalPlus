43ccfba0d946ff69d98ce6b96cef7b44bedc6540
Dobiasd <harry@daiw.de>
2015-12-16 09:36:12 +0100

    add github link to copyright notice

--- include/fplus/show.h
+++ include/fplus/show.h
@@ -1,4 +1,5 @@
 // Copyright Tobias Hermann 2015.
+// https://github.com/Dobiasd/FunctionalPlus
 // Distributed under the Boost Software License, Version 1.0.
 // (See accompanying file LICENSE_1_0.txt or copy at
 //  http://www.boost.org/LICENSE_1_0.txt)
