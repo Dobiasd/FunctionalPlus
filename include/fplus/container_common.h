5cb2647b1809eb34e1757c4ce4d536faf56bfc1b
Dobiasd <harry@daiw.de>
2016-05-06 23:31:37 +0200

    update copyright year

--- include/fplus/container_common.h
+++ include/fplus/container_common.h
@@ -1,4 +1,4 @@
-// Copyright Tobias Hermann 2015.
+// Copyright Tobias Hermann 2016.
 // https://github.com/Dobiasd/FunctionalPlus
 // Distributed under the Boost Software License, Version 1.0.
 // (See accompanying file LICENSE_1_0.txt or copy at
