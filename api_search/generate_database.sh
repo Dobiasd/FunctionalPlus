21892cb6182ffc0428dc05b5aaf6335113234ae6
Dobiasd <harry@daiw.de>
2016-05-09 20:57:30 +0200

    file rename

--- api_search/generate_database.sh
+++ api_search/generate_database.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-clang++-3.6 -O3 -std=c++11 -Wall -Wextra -pedantic -Werror -pthread -o ./temp_FunctionalPlus_api__clang -I./../include -lboost_filesystem -lboost_system generate_api_search_database.cpp
+clang++-3.6 -O3 -std=c++11 -Wall -Wextra -pedantic -Werror -pthread -o ./temp_FunctionalPlus_api__clang -I./../include -lboost_filesystem -lboost_system parse_source_files.cpp
 
 if [ -f ./temp_FunctionalPlus_api__clang ];
 then
