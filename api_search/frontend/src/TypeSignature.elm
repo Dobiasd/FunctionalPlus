6052e21fc0f6977a3312c24107bb07d2e3e0cdda
Dobiasd <harry@daiw.de>
2016-05-23 22:35:16 +0200

    fix: comments

--- api_search/frontend/src/TypeSignature.elm
+++ api_search/frontend/src/TypeSignature.elm
@@ -193,6 +193,6 @@ parseSignature =
     C.parse signatureParser
 
 
--- todo: fix parsing. Examples for current problems:
-    "A (B c) d" (incomplete parse)
-    "(a -> B c d)" (not idempotent)
+--todo: fix parsing. Examples for current problems:
+--    "A (B c) d" (incomplete parse)
+--    "(a -> B c d)" (not idempotent)
