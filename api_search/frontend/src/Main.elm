3eec8ea2289b3eb963e0144c2b922e89e4ca0a05
Dobiasd <harry@daiw.de>
2016-06-04 16:56:01 +0200

    join as search example

--- api_search/frontend/src/Main.elm
+++ api_search/frontend/src/Main.elm
@@ -238,7 +238,7 @@ view model =
                 div [ class "queryhelper" ]
                     [ text
                         ("search by function name, docs or type signature,"
-                            ++ " e.g. (string, vector<string>) -> string"
+                            ++ " e.g. (vector<string>, string) -> string"
                         )
                     ]
               else
