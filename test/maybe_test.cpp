ee66ca23162220755e78852bfede293ea338de11
Dobiasd <harry@daiw.de>
2016-06-19 11:42:36 +0200

    move maybe unit tests to google test

--- test/maybe_test.cpp
+++ test/maybe_test.cpp
@@ -69,8 +69,8 @@ TEST_F(maybe_test, just_with_default)
     auto x = just<int>(2);
     maybe<int> y = nothing<int>();
     auto Or42 = bind_1st_of_2(just_with_default<int>, 42);
-    assert(Or42(x) == 2);
-    assert(Or42(y) == 42);
+    EXPECT_THAT(Or42(x), Eq(2));
+    EXPECT_THAT(Or42(y), Eq(42));
 }
 
 TEST_F(maybe_test, lift)
@@ -79,10 +79,10 @@ TEST_F(maybe_test, lift)
     auto x = just<int>(2);
     maybe<int> y = nothing<int>();
     auto squareMaybe = lift_maybe(square);
-    assert(squareMaybe(x) == just(4));
-    assert(squareMaybe(y) == nothing<int>());
+    EXPECT_THAT(squareMaybe(x), Eq(just(4)));
+    EXPECT_THAT(squareMaybe(y), Eq(nothing<int>()));
     auto SquareAndSquare = compose(square, square);
-    assert((lift_maybe(SquareAndSquare))(x) == just(16));
+    EXPECT_THAT((lift_maybe(SquareAndSquare))(x), Eq(just(16)));
 }
 
 TEST_F(maybe_test, and_then)
@@ -90,15 +90,15 @@ TEST_F(maybe_test, and_then)
     using namespace fplus;
     auto sqrtAndSqrt = and_then_maybe(sqrtToMaybe, sqrtToMaybe);
     auto sqrtIntAndSqrtIntAndSqrtInt = and_then_maybe(sqrtToMaybeInt, sqrtToMaybeInt, sqrtToMaybeInt);
-    assert(sqrtIntAndSqrtIntAndSqrtInt(256) == just(2));
+    EXPECT_THAT(sqrtIntAndSqrtIntAndSqrtInt(256), Eq(just(2)));
     auto sqrtIntAndSqrtIntAndSqrtIntAndSqrtInt = and_then_maybe(sqrtToMaybeInt, sqrtToMaybeInt, sqrtToMaybeInt, sqrtToMaybeInt);
-    assert(sqrtIntAndSqrtIntAndSqrtIntAndSqrtInt(65536) == just(2));
+    EXPECT_THAT(sqrtIntAndSqrtIntAndSqrtIntAndSqrtInt(65536), Eq(just(2)));
 
     auto LiftedIntToFloat = lift_maybe(IntToFloat);
     auto JustInt = just<int>;
     auto IntToMaybeFloat = compose(JustInt, LiftedIntToFloat);
     auto IntToFloatAndSqrtAndSqrt = and_then_maybe(IntToMaybeFloat, sqrtAndSqrt);
-    assert(is_in_range(1.41f, 1.42f)(unsafe_get_just<float>
+    EXPECT_TRUE(is_in_range(1.41f, 1.42f)(unsafe_get_just<float>
             (IntToFloatAndSqrtAndSqrt(4))));
 }
 
@@ -106,28 +106,28 @@ TEST_F(maybe_test, equality)
 {
     using namespace fplus;
     IntMaybes maybes = {just(1), nothing<int>(), just(2)};
-    assert(justs(maybes) == Ints({ 1,2 }));
-    assert(just(1) == just(1));
-    assert(just(1) != just(2));
-    assert(just(1) != nothing<int>());
-    assert(nothing<int>() == nothing<int>());
+    EXPECT_TRUE(justs(maybes) == Ints({ 1,2 }));
+    EXPECT_TRUE(just(1) == just(1));
+    EXPECT_TRUE(just(1) != just(2));
+    EXPECT_TRUE(just(1) != nothing<int>());
+    EXPECT_TRUE(nothing<int>() == nothing<int>());
 }
 
 TEST_F(maybe_test, transform_and_keep_justs)
 {
     using namespace fplus;
     Ints wholeNumbers = { -3, 4, 16, -1 };
-    assert(transform_and_keep_justs(sqrtToMaybeInt, wholeNumbers)
-            == Ints({2,4}));
-    assert(transform_and_concat(bind_1st_of_2(replicate<int>, 3), Ints{ 1,2 })
-            == Ints({ 1,1,1,2,2,2 }));
+    EXPECT_THAT(transform_and_keep_justs(sqrtToMaybeInt, wholeNumbers)
+           , Eq(Ints({2,4})));
+    EXPECT_THAT(transform_and_concat(bind_1st_of_2(replicate<int>, 3), Ints{ 1,2 })
+           , Eq(Ints({ 1,1,1,2,2,2 })));
 }
 
 TEST_F(maybe_test, show_maybe)
 {
     using namespace fplus;
-    assert(show_maybe(just<int>(42)) == std::string("Just 42"));
-    assert(show_maybe(nothing<int>()) == std::string("Nothing"));
+    EXPECT_THAT(show_maybe(just<int>(42)), Eq(std::string("Just 42")));
+    EXPECT_THAT(show_maybe(nothing<int>()), Eq(std::string("Nothing")));
 }
 
 TEST_F(maybe_test, exceptions)
@@ -142,7 +142,7 @@ TEST_F(maybe_test, exceptions)
     {
         thrown_str = e.what();
     }
-    assert(thrown_str == "raised");
+    EXPECT_THAT(thrown_str, Eq("raised"));
 }
 
 TEST_F(maybe_test, copy)
@@ -152,7 +152,7 @@ TEST_F(maybe_test, copy)
     maybe<int> maybe_4_copy(maybe_4);
     maybe<int> maybe_4_copy_2;
     maybe_4_copy_2 = maybe_4_copy;
-    assert(maybe_4_copy_2 == just<int>(4));
+    EXPECT_THAT(maybe_4_copy_2, Eq(just<int>(4)));
 }
 
 TEST_F(maybe_test, flatten)
@@ -162,8 +162,8 @@ TEST_F(maybe_test, flatten)
     maybe<int> maybe_int_nothing_copy(maybe_int_nothing);
     maybe<int> maybe_int_nothing_copy_2;
     maybe_int_nothing_copy_2 = maybe_int_nothing_copy;
-    assert(maybe_int_nothing_copy_2 == nothing<int>());
-    assert(flatten_maybe(maybe<maybe<int>>(maybe<int>(1))) == maybe<int>(1));
-    assert(flatten_maybe(maybe<maybe<int>>(maybe<int>())) == nothing<int>());
-    assert(flatten_maybe(maybe<maybe<int>>()) == nothing<int>());
+    EXPECT_THAT(maybe_int_nothing_copy_2, Eq(nothing<int>()));
+    EXPECT_THAT(flatten_maybe(maybe<maybe<int>>(maybe<int>(1))), Eq(maybe<int>(1)));
+    EXPECT_THAT(flatten_maybe(maybe<maybe<int>>(maybe<int>())), Eq(nothing<int>()));
+    EXPECT_THAT(flatten_maybe(maybe<maybe<int>>()), Eq(nothing<int>()));
 }
