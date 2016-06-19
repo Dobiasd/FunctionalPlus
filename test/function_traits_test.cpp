b099b1be821f3840672abcb2e7421a694218db4d
Dobiasd <harry@daiw.de>
2016-06-19 11:23:20 +0200

    move function traits unit tests to google test

---
+++ test/function_traits_test.cpp
@@ -0,0 +1,141 @@
+
+#include <gtest/gtest.h>
+#include <gmock/gmock.h>
+#include "fplus/fplus.h"
+#include <vector>
+
+using namespace testing;
+
+class function_traits_test : public testing::Test
+{
+protected:
+
+    void SetUp() override
+    {
+    }
+
+    void TearDown() override
+    {
+    }
+
+    static int APlusTwoTimesBFunc(int a, int b)
+    {
+        return a + 2 * b;
+    }
+
+    typedef std::deque<int> IntDeq;
+    typedef std::deque<IntDeq> IntContCont;
+    typedef IntDeq IntCont;
+    typedef IntCont Row;
+};
+
+std::string CcI2SFree(const std::string& str, int x)
+{
+    return str + std::to_string(x);
+}
+
+auto CcI2SLambda = [](const std::string& str, int x)
+{ return CcI2SFree(str, x); };
+
+std::function<std::string(const std::string&, int)>
+CcI2SStdFunction = CcI2SLambda;
+
+std::string (*CcI2SFunctionPointer)(const std::string&, int) =
+&CcI2SFree;
+
+struct CcI2SStrct {
+    std::string operator() (const std::string& str, int x)
+    { return CcI2SFree(str, x); }
+    std::string nonCMemF (const std::string& str, int x)
+    { return CcI2SFree(str, x); }
+    std::string cnstMemF (const std::string& str, int x) const
+    { return CcI2SFree(str, x); }
+    static std::string sttcMemF (const std::string& str, int x)
+    { return CcI2SFree(str, x); }
+};
+
+TEST_F(function_traits_test, static_asserts)
+{
+    using namespace fplus;
+    static_assert(std::is_same<
+        utils::function_traits<decltype(CcI2SFree)>::arg<0>::type,
+        const std::string&>::value, "No.");
+    static_assert(std::is_same<
+        utils::function_traits<decltype(CcI2SFree)>::arg<1>::type,
+        int>::value, "No.");
+    static_assert(std::is_same<
+        utils::function_traits<decltype(CcI2SFree)>::result_type,
+        std::string>::value, "No.");
+
+    static_assert(std::is_same<
+        utils::function_traits<decltype(CcI2SLambda)>::arg<0>::type,
+        const std::string&>::value, "No.");
+    static_assert(std::is_same<
+        utils::function_traits<decltype(CcI2SLambda)>::arg<1>::type,
+        int>::value, "No.");
+    static_assert(std::is_same<
+        utils::function_traits<decltype(CcI2SLambda)>::result_type,
+        std::string>::value, "No.");
+
+    static_assert(std::is_same<
+        utils::function_traits<decltype(CcI2SStdFunction)>::arg<0>::type,
+        const std::string&>::value, "No.");
+    static_assert(std::is_same<
+        utils::function_traits<decltype(CcI2SStdFunction)>::arg<1>::type,
+        int>::value, "No.");
+    static_assert(std::is_same<
+        utils::function_traits<decltype(CcI2SStdFunction)>::result_type,
+        std::string>::value, "No.");
+
+    static_assert(std::is_same<
+        utils::function_traits<decltype(CcI2SFunctionPointer)>::arg<0>::type,
+        const std::string&>::value, "No.");
+    static_assert(std::is_same<
+        utils::function_traits<decltype(CcI2SFunctionPointer)>::arg<1>::type,
+        int>::value, "No.");
+    static_assert(std::is_same<
+        utils::function_traits<decltype(CcI2SFunctionPointer)>::result_type,
+        std::string>::value, "No.");
+
+    CcI2SStrct ccI2SStrct;
+    ccI2SStrct("dummy call to avoid unused variable warnings", 0);
+    static_assert(std::is_same<
+        utils::function_traits<decltype(ccI2SStrct)>::arg<0>::type,
+        const std::string&>::value, "No.");
+    static_assert(std::is_same<
+        utils::function_traits<decltype(ccI2SStrct)>::arg<1>::type,
+        int>::value, "No.");
+    static_assert(std::is_same<
+        utils::function_traits<decltype(ccI2SStrct)>::result_type,
+        std::string>::value, "No.");
+
+    static_assert(std::is_same<
+        utils::function_traits<decltype(&CcI2SStrct::nonCMemF)>::arg<0>::type,
+        const std::string&>::value, "No.");
+    static_assert(std::is_same<
+        utils::function_traits<decltype(&CcI2SStrct::nonCMemF)>::arg<1>::type,
+        int>::value, "No.");
+    static_assert(std::is_same<
+        utils::function_traits<decltype(&CcI2SStrct::nonCMemF)>::result_type,
+        std::string>::value, "No.");
+
+    static_assert(std::is_same<
+        utils::function_traits<decltype(&CcI2SStrct::cnstMemF)>::arg<0>::type,
+        const std::string&>::value, "No.");
+    static_assert(std::is_same<
+        utils::function_traits<decltype(&CcI2SStrct::cnstMemF)>::arg<1>::type,
+        int>::value, "No.");
+    static_assert(std::is_same<
+        utils::function_traits<decltype(&CcI2SStrct::cnstMemF)>::result_type,
+        std::string>::value, "No.");
+
+    static_assert(std::is_same<
+        utils::function_traits<decltype(&CcI2SStrct::sttcMemF)>::arg<0>::type,
+        const std::string&>::value, "No.");
+    static_assert(std::is_same<
+        utils::function_traits<decltype(&CcI2SStrct::sttcMemF)>::arg<1>::type,
+        int>::value, "No.");
+    static_assert(std::is_same<
+        utils::function_traits<decltype(&CcI2SStrct::sttcMemF)>::result_type,
+        std::string>::value, "No.");
+}
--- test/tests.cpp
+++ test/tests.cpp
@@ -18,116 +18,6 @@
 #include <string>
 #include <vector>
 
-std::string CcI2SFree(const std::string& str, int x)
-{
-    return str + std::to_string(x);
-}
-
-auto CcI2SLambda = [](const std::string& str, int x)
-{ return CcI2SFree(str, x); };
-
-std::function<std::string(const std::string&, int)>
-CcI2SStdFunction = CcI2SLambda;
-
-std::string (*CcI2SFunctionPointer)(const std::string&, int) =
-&CcI2SFree;
-
-struct CcI2SStrct {
-    std::string operator() (const std::string& str, int x)
-    { return CcI2SFree(str, x); }
-    std::string nonCMemF (const std::string& str, int x)
-    { return CcI2SFree(str, x); }
-    std::string cnstMemF (const std::string& str, int x) const
-    { return CcI2SFree(str, x); }
-    static std::string sttcMemF (const std::string& str, int x)
-    { return CcI2SFree(str, x); }
-};
-
-void Test_FunctionTraits()
-{
-    static_assert(std::is_same<
-        utils::function_traits<decltype(CcI2SFree)>::arg<0>::type,
-        const std::string&>::value, "No.");
-    static_assert(std::is_same<
-        utils::function_traits<decltype(CcI2SFree)>::arg<1>::type,
-        int>::value, "No.");
-    static_assert(std::is_same<
-        utils::function_traits<decltype(CcI2SFree)>::result_type,
-        std::string>::value, "No.");
-
-    static_assert(std::is_same<
-        utils::function_traits<decltype(CcI2SLambda)>::arg<0>::type,
-        const std::string&>::value, "No.");
-    static_assert(std::is_same<
-        utils::function_traits<decltype(CcI2SLambda)>::arg<1>::type,
-        int>::value, "No.");
-    static_assert(std::is_same<
-        utils::function_traits<decltype(CcI2SLambda)>::result_type,
-        std::string>::value, "No.");
-
-    static_assert(std::is_same<
-        utils::function_traits<decltype(CcI2SStdFunction)>::arg<0>::type,
-        const std::string&>::value, "No.");
-    static_assert(std::is_same<
-        utils::function_traits<decltype(CcI2SStdFunction)>::arg<1>::type,
-        int>::value, "No.");
-    static_assert(std::is_same<
-        utils::function_traits<decltype(CcI2SStdFunction)>::result_type,
-        std::string>::value, "No.");
-
-    static_assert(std::is_same<
-        utils::function_traits<decltype(CcI2SFunctionPointer)>::arg<0>::type,
-        const std::string&>::value, "No.");
-    static_assert(std::is_same<
-        utils::function_traits<decltype(CcI2SFunctionPointer)>::arg<1>::type,
-        int>::value, "No.");
-    static_assert(std::is_same<
-        utils::function_traits<decltype(CcI2SFunctionPointer)>::result_type,
-        std::string>::value, "No.");
-
-    CcI2SStrct ccI2SStrct;
-    ccI2SStrct("dummy call to avoid unused variable warnings", 0);
-    static_assert(std::is_same<
-        utils::function_traits<decltype(ccI2SStrct)>::arg<0>::type,
-        const std::string&>::value, "No.");
-    static_assert(std::is_same<
-        utils::function_traits<decltype(ccI2SStrct)>::arg<1>::type,
-        int>::value, "No.");
-    static_assert(std::is_same<
-        utils::function_traits<decltype(ccI2SStrct)>::result_type,
-        std::string>::value, "No.");
-
-    static_assert(std::is_same<
-        utils::function_traits<decltype(&CcI2SStrct::nonCMemF)>::arg<0>::type,
-        const std::string&>::value, "No.");
-    static_assert(std::is_same<
-        utils::function_traits<decltype(&CcI2SStrct::nonCMemF)>::arg<1>::type,
-        int>::value, "No.");
-    static_assert(std::is_same<
-        utils::function_traits<decltype(&CcI2SStrct::nonCMemF)>::result_type,
-        std::string>::value, "No.");
-
-    static_assert(std::is_same<
-        utils::function_traits<decltype(&CcI2SStrct::cnstMemF)>::arg<0>::type,
-        const std::string&>::value, "No.");
-    static_assert(std::is_same<
-        utils::function_traits<decltype(&CcI2SStrct::cnstMemF)>::arg<1>::type,
-        int>::value, "No.");
-    static_assert(std::is_same<
-        utils::function_traits<decltype(&CcI2SStrct::cnstMemF)>::result_type,
-        std::string>::value, "No.");
-
-    static_assert(std::is_same<
-        utils::function_traits<decltype(&CcI2SStrct::sttcMemF)>::arg<0>::type,
-        const std::string&>::value, "No.");
-    static_assert(std::is_same<
-        utils::function_traits<decltype(&CcI2SStrct::sttcMemF)>::arg<1>::type,
-        int>::value, "No.");
-    static_assert(std::is_same<
-        utils::function_traits<decltype(&CcI2SStrct::sttcMemF)>::result_type,
-        std::string>::value, "No.");
-}
-
 void Test_Maybe()
 {
     using namespace fplus;
@@ -1248,10 +1138,6 @@ void Test_VariadicNumericTools()
 int main()
 {
     std::cout << "Running all tests." << std::endl;
-
-    std::cout << "Testing FunctionTraits." << std::endl;
-    Test_FunctionTraits();
-    std::cout << "FunctionTraits OK." << std::endl;
 
     std::cout << "Testing Maybe." << std::endl;
     Test_Maybe();
