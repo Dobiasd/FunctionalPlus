9657cb726c0e4c81b340ab365eea4b90f200e306
Dobiasd <harry@daiw.de>
2016-12-02 15:03:59 +0100

    more interpolation tests

--- test/interpolate_test.cpp
+++ test/interpolate_test.cpp
@@ -18,14 +18,15 @@ namespace {
 TEST_CASE("interpolate_test, interpolate_linear")
 {
     using namespace fplus;
-    REQUIRE(is_in_range_around(0.01, 1.0)(elem_at_float_idx(-1.7, xs)));
-    REQUIRE(is_in_range_around(0.01, 1.0)(elem_at_float_idx(-0.2, xs)));
-    REQUIRE(is_in_range_around(0.01, 1.0)(elem_at_float_idx( 0.0, xs)));
-    REQUIRE(is_in_range_around(0.01, 1.2)(elem_at_float_idx( 0.2, xs)));
-    REQUIRE(is_in_range_around(0.01, 1.7)(elem_at_float_idx( 0.7, xs)));
-    REQUIRE(is_in_range_around(0.01, 4.0)(elem_at_float_idx( 1.5, xs)));
-    REQUIRE(is_in_range_around(0.01, 5.6)(elem_at_float_idx( 1.9, xs)));
-    REQUIRE(is_in_range_around(0.01, 6.0)(elem_at_float_idx( 2.0, xs)));
-    REQUIRE(is_in_range_around(0.01, 6.0)(elem_at_float_idx( 2.1, xs)));
-    REQUIRE(is_in_range_around(0.01, 6.0)(elem_at_float_idx( 2.8, xs)));
+    REQUIRE(is_in_range_around(0.0001, 1.0)(elem_at_float_idx(-1.7, xs)));
+    REQUIRE(is_in_range_around(0.0001, 1.0)(elem_at_float_idx(-0.2, xs)));
+    REQUIRE(is_in_range_around(0.0001, 1.0)(elem_at_float_idx( 0.0, xs)));
+    REQUIRE(is_in_range_around(0.0001, 1.2)(elem_at_float_idx( 0.2, xs)));
+    REQUIRE(is_in_range_around(0.0001, 1.7)(elem_at_float_idx( 0.7, xs)));
+    REQUIRE(is_in_range_around(0.0001, 2.0)(elem_at_float_idx( 1.0, xs)));
+    REQUIRE(is_in_range_around(0.0001, 4.0)(elem_at_float_idx( 1.5, xs)));
+    REQUIRE(is_in_range_around(0.0001, 5.6)(elem_at_float_idx( 1.9, xs)));
+    REQUIRE(is_in_range_around(0.0001, 6.0)(elem_at_float_idx( 2.0, xs)));
+    REQUIRE(is_in_range_around(0.0001, 6.0)(elem_at_float_idx( 2.1, xs)));
+    REQUIRE(is_in_range_around(0.0001, 6.0)(elem_at_float_idx( 2.8, xs)));
 }
