1b1a1baa959bac027fb92ce3827cafaa5e3ca7c1
Tobias Hermann <editgym@gmail.com>
2016-12-19 22:52:18 +0100

    Use dev version of lib for API search database generation

--- api_search/generate_database.sh
+++ api_search/generate_database.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
-clang++ -O3 -std=c++11 -Wall -Wextra -pedantic -Wshadow -Werror -Weffc++ -Wconversion -Wsign-conversion -Wctor-dtor-privacy -Wreorder -Wold-style-cast -Wparentheses -pthread -o ./temp_FunctionalPlus_api__clang -I./../include parse_source_files.cpp
+clang++ -O3 -std=c++11 -Wall -Wextra -pedantic -Wshadow -Werror -Weffc++ -Wconversion -Wsign-conversion -Wctor-dtor-privacy -Wreorder -Wold-style-cast -Wparentheses -pthread -o ./temp_FunctionalPlus_api__clang -I../include parse_source_files.cpp
 
 if [ -f ./temp_FunctionalPlus_api__clang ];
 then
-    g++ -E -CC -std=c++11 ../include ../include/fplus/fplus.hpp > temp_preprocessor_output.txt
+    g++ -E -CC -std=c++11 -I../include ../include/fplus/fplus.hpp > temp_preprocessor_output.txt
     ./temp_FunctionalPlus_api__clang temp_preprocessor_output.txt
     rm ./temp_FunctionalPlus_api__clang
     rm -f temp_preprocessor_output.txt
