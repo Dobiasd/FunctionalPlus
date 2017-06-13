d33f53af48311aa181b2946e744aab37f3576fe3
Dobiasd <editgym@gmail.com>
2017-06-13 15:34:09 +0100

    refactoring: extract common frontend functions

--- api_search/frontend/src/Main.elm
+++ api_search/frontend/src/Main.elm
@@ -1,6 +1,6 @@
 module FPlusApiSearch exposing (..)
 
-import Database
+import FPlusApiCommon exposing (..)
 import TypeSignature
 import Html exposing (..)
 import Html.Attributes exposing (..)
@@ -29,82 +29,6 @@ initModelAndCommands =
     ( defaultModel, Cmd.none )
 
 
-type alias WithParsedSignature a =
-    { a
-        | parsedSignature : TypeSignature.Signature
-        , signatureFwd : Maybe String
-        , parsedSignatureFwd : Maybe TypeSignature.Signature
-    }
-
-
-type alias Function =
-    WithParsedSignature Database.Function
-
-
-parseSignatureCrashOnError :
-    Database.Function
-    -> TypeSignature.Signature
-parseSignatureCrashOnError function =
-    case
-        TypeSignature.parseSignature function.signature
-    of
-        Just sig ->
-            sig
-
-        Nothing ->
-            "Error parsing signature of "
-                ++ function.name
-                ++ ": "
-                ++ function.signature
-                |> Debug.crash
-
-
-hasFwdSignature : String -> Bool
-hasFwdSignature documentation =
-    String.contains "fwd bind count" documentation
-
-
-removeFwdBindCount : String -> String
-removeFwdBindCount documentation =
-    documentation
-        |> String.lines
-        |> List.filter (\x -> String.contains "fwd bind count" x |> not)
-        |> String.join "\n"
-
-
-addParsedSignatureToFunction : Database.Function -> Function
-addParsedSignatureToFunction function =
-    let
-        parsedSig =
-            parseSignatureCrashOnError function
-
-        parsedSigFwd =
-            if hasFwdSignature function.documentation then
-                parsedSig
-                    |> TypeSignature.curry1
-                    |> Maybe.Just
-            else
-                Maybe.Nothing
-    in
-        { name = function.name
-        , signature = function.signature
-        , parsedSignature = parsedSig |> TypeSignature.normalizeSignature
-        , parsedSignatureFwd =
-            parsedSigFwd
-                |> Maybe.map TypeSignature.normalizeSignature
-        , signatureFwd =
-            parsedSigFwd
-                |> Maybe.map (TypeSignature.showSignature True)
-        , documentation = removeFwdBindCount function.documentation
-        , declaration = function.declaration
-        }
-
-
-functions : List Function
-functions =
-    List.map addParsedSignatureToFunction Database.functions
-
-
 type alias Model =
     { query : String
     , querySigStr : String
@@ -118,11 +42,6 @@ defaultModel =
     , querySigStr = ""
     , searchResult = []
     }
-
-
-type Msg
-    = NoOp
-    | UpdateQuery String
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
@@ -187,16 +106,6 @@ update action model =
                     )
 
 
-showMaybeSig : Maybe TypeSignature.Signature -> String
-showMaybeSig maybeSig =
-    case maybeSig of
-        Maybe.Just s ->
-            TypeSignature.showSignature True s
-
-        Maybe.Nothing ->
-            ""
-
-
 view : Model -> Html Msg
 view model =
     div [ class "mainwrapper" ]
@@ -248,94 +157,6 @@ showFooter =
         ]
 
 
-replaceInString : String -> String -> String -> String
-replaceInString pattern replacement =
-    Regex.replace Regex.All (Regex.regex pattern) (always replacement)
-
-
-replaceSubMatchInString : String -> (String -> String) -> String -> String
-replaceSubMatchInString pattern replacementFunc =
-    let
-        f { match, submatches } =
-            case submatches of
-                [ Maybe.Just sm ] ->
-                    replacementFunc sm
-
-                _ ->
-                    match
-    in
-        Regex.replace Regex.All (Regex.regex pattern) f
-
-
-replaceTwoSubMatchInString :
-    String
-    -> (String -> String -> String)
-    -> String
-    -> String
-replaceTwoSubMatchInString pattern replacementFunc =
-    let
-        f { match, submatches } =
-            case submatches of
-                [ Maybe.Just sm1, Maybe.Just sm2 ] ->
-                    replacementFunc sm1 sm2
-
-                _ ->
-                    match
-    in
-        Regex.replace Regex.All (Regex.regex pattern) f
-
-
-applyUntilIdempotent : (String -> String) -> String -> String
-applyUntilIdempotent f str =
-    let
-        res =
-            f str
-    in
-        if res == str then
-            res
-        else
-            applyUntilIdempotent f res
-
-
-cleanFunctionSignature : String -> String
-cleanFunctionSignature =
-    let
-        recursiveCases =
-            replaceSubMatchInString "vector<([^<>]*)>" (\sm -> "[" ++ sm ++ "]")
-                >> replaceSubMatchInString "list<([^<>]*)>" (\sm -> "[" ++ sm ++ "]")
-                >> replaceSubMatchInString "set<([^<>]*)>" (\sm -> "Set " ++ sm)
-                >> replaceTwoSubMatchInString "map<([^<>]*),([^<>]*)>" (\sm1 sm2 -> "Map " ++ sm1 ++ " " ++ sm2)
-                >> replaceTwoSubMatchInString "pair<([^<>]*),([^<>]*)>" (\sm1 sm2 -> "(" ++ sm1 ++ ", " ++ sm2 ++ ")")
-                |> applyUntilIdempotent
-    in
-        String.toLower
-            >> replaceInString "std::" ""
-            >> replaceInString "fplus::" ""
-            >> recursiveCases
-            >> replaceInString "unsigned int" "Int"
-            >> replaceInString "unsigned" "Int"
-            >> replaceInString "size_t" "Int"
-            >> replaceInString "short" "Int"
-            >> replaceInString "signed" "Int"
-            >> replaceInString "long" "Int"
-            >> replaceInString "integer" "Int"
-            >> replaceInString "int" "Int"
-            >> replaceInString "double" "Float"
-            >> replaceInString "float" "Float"
-            >> replaceInString "boolean" "Bool"
-            >> replaceInString "char" "Char"
-            >> replaceInString "bool" "Bool"
-            >> replaceInString "io" "Io"
-            >> replaceInString "maybe" "Maybe"
-            >> replaceInString "either" "Result"
-            >> replaceInString "result" "Result"
-            >> replaceInString "map" "Map"
-            >> replaceInString "dict" "Map"
-            >> replaceInString "set" "Set"
-            >> replaceInString "string" "String"
-            >> replaceInString "String" "[Char]"
-
-
 searchFunctions :
     String
     -> Maybe TypeSignature.Signature
@@ -351,14 +172,6 @@ searchFunctions query querySig querySigLower =
             |> List.filter (\( _, rating ) -> rating > 0)
             |> List.sortBy (\( _, rating ) -> 0 - rating)
             |> List.take maxVisibleFunctions
-
-
-boolToNum : Float -> Bool -> Float
-boolToNum value b =
-    if b then
-        value
-    else
-        0
 
 
 typeRating :
@@ -379,16 +192,6 @@ adjustQuery query =
 
         _ ->
             query
-
-
-maybeSigIsArrow : Maybe TypeSignature.Signature -> Bool
-maybeSigIsArrow maybeSig =
-    case maybeSig of
-        Maybe.Just sig ->
-            TypeSignature.sigIsArrow sig
-
-        _ ->
-            False
 
 
 functionRating :
@@ -510,30 +313,6 @@ showFunctions ratedFunctions =
         |> div [ class "functions" ]
 
 
-stringToCode : String -> String -> Html Msg
-stringToCode language str =
-    let
-        defOpts =
-            Markdown.defaultOptions
-
-        options =
-            { defOpts | defaultHighlighting = Just language }
-
-        taggedStr =
-            "```\n" ++ str ++ "\n```"
-    in
-        Markdown.toHtmlWith options [] taggedStr
-
-
-docFromString : String -> Html Msg
-docFromString str =
-    let
-        taggedStr =
-            "```" ++ str ++ "```"
-    in
-        Markdown.toHtmlWith Markdown.defaultOptions [] taggedStr
-
-
 ratingToHtml : Float -> Html Msg
 ratingToHtml rating =
     "search rating: " ++ toString (round rating) |> docFromString
@@ -542,40 +321,6 @@ ratingToHtml rating =
 showRatedFunction : ( Function, Float ) -> Html Msg
 showRatedFunction ( function, rating ) =
     let
-        functionNameAndSig =
-            div [ class "functionnameandsig" ]
-                [ function.name
-                    ++ " : "
-                    ++ function.signature
-                    |> stringToCode "haskell"
-                ]
-
-        functionNameAndSigFwd =
-            case function.signatureFwd of
-                Maybe.Just sigFwd ->
-                    div [ class "functionnameandsig" ]
-                        [ "fwd::"
-                            ++ function.name
-                            ++ " : "
-                            ++ sigFwd
-                            |> stringToCode "haskell"
-                        ]
-
-                Maybe.Nothing ->
-                    div [] []
-
-        functionDocumentation =
-            div [ class "functiondoc" ]
-                [ function.documentation
-                    |> docFromString
-                ]
-
-        functionDeclaration =
-            div [ class "functiondecl" ]
-                [ function.declaration
-                    |> stringToCode "cpp"
-                ]
-
         functionRating =
             div [ class "functionrating" ]
                 [ rating
@@ -583,9 +328,4 @@ showRatedFunction ( function, rating ) =
                 ]
     in
         div [ class "function" ]
-            [ functionNameAndSig
-            , functionNameAndSigFwd
-            , functionDocumentation
-            , functionDeclaration
-            , functionRating
-            ]
+            ( showFunctionDivs function ++ [functionRating] )
