cb433e8be2925a3ede05278a103c404bc25fe427
Dobiasd <editgym@gmail.com>
2017-08-29 08:28:59 +0200

    match libc++ versions to clang versions

--- build_and_test.sh
+++ build_and_test.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 
 if [[ "$CXX" == clang* ]]; then
-    sudo apt-add-repository "deb http://llvm.org/apt/trusty/ llvm-toolchain-trusty-3.9 main"
-    sudo apt-get update
-    sudo apt-get install -y libc++-dev libc++-helpers libc++1 libc++abi-dev lldb-3.9
     export CXXFLAGS="-stdlib=libc++"
 fi
 
