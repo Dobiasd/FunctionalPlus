0ed0d8b99d95a252167b0e8834ed5a8e25f1fb23
Dobiasd <editgym@gmail.com>
2017-09-15 20:31:02 +0200

    inline before_install.sh

--- before_install.sh
+++
@@ -1,8 +0,0 @@
-wget https://raw.githubusercontent.com/onqtam/doctest/master/doctest/doctest.h
-sudo mv ./doctest.h /usr/local/include/doctest.h
-
-which $CXX
-which $CC
-which valgrind
-$CXX --version
-if [ -n "$CLANG_VERSION" ]; then sudo PATH="${PATH}" CXX="$CXX" CC="$CC" ./install_libcxx.sh; fi
