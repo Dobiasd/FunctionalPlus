fd95da634cd12e300baa24a577ec1b72b8a780e0
Théo DELRIEU <theo.delrieu@tanker.io>
2017-09-25 14:21:05 +0200

    generate_test.cpp: use generic lambdas in every relevant test

--- test/generate_test.cpp
+++ test/generate_test.cpp
@@ -12,15 +12,15 @@
 
 namespace {
 
-    bool compare_not_eq(char c1, char c2)
+    auto compare_not_eq = [](auto c1, auto c2)
     {
         return c1 != c2;
-    }
+    };
 
-    std::pair<char, char> as_pair(char c1, char c2)
+    auto as_pair = [](auto c1, auto c2)
     {
         return std::make_pair(c1, c2);
-    }
+    };
 
 }
 
@@ -206,7 +206,7 @@ TEST_CASE("generate_test, power_set")
 
 TEST_CASE("generate_test, iterate")
 {
-    auto f = [](int value) { return value * 2; };
+    auto f = [](auto value) { return value * 2; };
     auto result = fplus::iterate(f, 5, 3);
     REQUIRE_EQ(5u, result.size());
     REQUIRE_EQ(result, std::vector<int>({3, 6, 12, 24, 48}));
@@ -214,14 +214,14 @@ TEST_CASE("generate_test, iterate")
 
 TEST_CASE("generate_test, iterate_with_size_0")
 {
-    auto f = [](int value) { return value * 2; };
+    auto f = [](auto value) { return value * 2; };
     auto result = fplus::iterate(f, 0, 3);
     REQUIRE(result.empty());
 }
 
 TEST_CASE("generate_test, iterate_maybe")
 {
-    const auto next_collatz_value = [](int x) -> fplus::maybe<int>
+    const auto next_collatz_value = [](auto x) -> fplus::maybe<int>
     {
         if (x <= 1)
             return {};
