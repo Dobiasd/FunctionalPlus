7ef8ac9b6dee4631edd9dcaf135502330f4e3a07
Artalus <artalus-mail@yandex.ru>
2017-12-19 17:17:11 +0300

    swap to_lower/upper_case parameters

--- test/stringtools_utf8_test.cpp
+++ test/stringtools_utf8_test.cpp
@@ -14,12 +14,12 @@ TEST_CASE("stringtools_test, to_lower/upper_case, utf8")
 {
     using namespace fplus;
     const std::locale loc_utf8("ru_RU.utf8");
-    auto lower = fwd::flip::to_lower_case_loc(loc_utf8);
-    auto upper = fwd::flip::to_upper_case_loc(loc_utf8);
+    auto lower = fwd::to_lower_case_loc(loc_utf8);
+    auto upper = fwd::to_upper_case_loc(loc_utf8);
     REQUIRE_EQ(lower(std::wstring(L"cYrIlLiC 123&? КиРиЛлИцА")), std::wstring(L"cyrillic 123&? кириллица"));
     REQUIRE_EQ(lower(std::wstring(L"АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ")), std::wstring(L"абвгдеёжзийклмнопрстуфхцчшщъыьэюя"));
 
-    REQUIRE_EQ(lower(std::wstring(L"cYrIlLiC 123&? КиРиЛлИцА")), std::wstring(L"CYRILLIC 123&? КИРИЛЛИЦА"));
-    REQUIRE_EQ(lower(std::wstring(L"абвгдеёжзийклмнопрстуфхцчшщъыьэюя")), std::wstring(L"АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ"));
+    REQUIRE_EQ(upper(std::wstring(L"cYrIlLiC 123&? КиРиЛлИцА")), std::wstring(L"CYRILLIC 123&? КИРИЛЛИЦА"));
+    REQUIRE_EQ(upper(std::wstring(L"абвгдеёжзийклмнопрстуфхцчшщъыьэюя")), std::wstring(L"АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ"));
 }
 #endif
