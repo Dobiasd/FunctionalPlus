c1ff0b9943efb19aefbd65678a2e4f2aa3615c4f
Pascal Thomet <pthomet@gmail.com>
2018-10-09 09:54:31 +0200

    benchmark_session_test.cpp add example

--- test/benchmark_session_test.cpp
+++ test/benchmark_session_test.cpp
@@ -11,55 +11,115 @@
 #include <fplus/fplus.hpp>
 #include <fplus/benchmark_session.hpp>
 
+
+// This is an example on how to use benchmark_session in order to bench separate parts of an algorithm
+
+// We need to instantiate a session into which the stats will be collected
 fplus::benchmark_session my_benchmark_session;
 
 
-// In this example, we are benchmarking gemstones_bench, 
-// and only benchmark 2 sub-functions : split_lines and convert_container
-std::string gemstones_bench(const std::string& input)
+// Benchmarked function : several sub parts of this function are benchmarked separately
+int benchmark_example()
 {
-    using namespace fplus;
+    using Ints = std::vector<int>;
 
-    typedef std::set<std::string::value_type> character_set;
+    // Example 1 : benchmark by replacing a function
+    //
+    // We want to benchmark the following code :
+    //    Ints ascending_numbers = fplus::numbers(0, 1000);
+    //
+    // So, first we make an alternate version of the function "fplus::numbers"
+    // Since fplus::numbers is a template function, we need to specify 
+    // that the actual version we want to benchmark 
+    // is "fplus::numbers<int, std::vector<int>>"
+    //
+    // numbers_bench will our alternate version and it
+    // has the same signature as fplus::numbers<int, std::vector<int>>, 
+    // except that it also stores stats into the benchmark session, 
+    // under the name "numbers"
+    //
+    // Note that make_benchmark_function *will add side effects* to the function
+    // (since it stores data into the benchmark session at each call)
+    auto numbers_bench = make_benchmark_function(
+        my_benchmark_session,
+        "numbers",
+        fplus::numbers<int, std::vector<int>> 
+    );
+    // Then, we replace the original code "Ints ascending_numbers = fplus::numbers(0, 1000);"
+    // by a code that uses the benchmarked function
+    Ints ascending_numbers = numbers_bench(0, 100000);
 
-    // Benchmark by replacing a function:
-    // Below, we will benchmark the call to split_lines:
-    // // const auto lines = split_lines(false, input); // this was the original code 
-    // 1. we define an alternate version split_lines_bench
-    auto split_lines_bench = make_benchmark_function(my_benchmark_session, "split_lines", split_lines<std::string>);
-    // 2. we replace the call by using the alternate version
-    const auto lines = split_lines_bench(false, input); // replace call with benchmarked version
-
-    // Benchmark by replacing an expression
+    // Example 2: benchmark by replacing an expression
     // Below, we will benchmark an expression
-    // The original expression was:
-    // const auto sets = transform( convert_container<character_set, std::string>, lines);
-    // We just copy paste this expression into the bench_expression macro, like shown below
-    // Note: since this is a macro we must replace the ',' by COMMA
-    const auto sets = benchmark_expression(
-        my_benchmark_session, 
-        "transform", 
-        transform(convert_container<character_set, std::string> COMMA lines)
+    // The original expression we want to benchmark was:
+    //     Ints shuffled_numbers = fplus::shuffle(std::mt19937::default_seed, ascending_numbers);
+    //
+    // In order to do so, we just copy/paste this expression 
+    // into "bench_expression" like shown below.
+    // This expression will then be benchmarked with the name "shuffle"
+    //
+    // Notes :
+    //  - benchmark_expression is a preprocessor macro that uses an immediately invoked lambda (IIL)
+    // - the expression can be paster as-is, and it is possible to not remove the ";"
+    //   (although it also works if it is not present)
+    Ints shuffled_numbers = benchmark_expression(
+        my_benchmark_session,
+        "shuffle",
+        fplus::shuffle(std::mt19937::default_seed, ascending_numbers);
     );
 
-    // Build the intersection of all character sets.
-    const auto gem_elements = fold_left_1(
-        set_intersection<character_set>, sets);
+    // Example 3: also benchmark by replacing an expression
+    // The original expression was
+    //    const auto sorted_numbers = fplus::sort(shuffled_numbers);
+    const auto sorted_numbers = benchmark_expression(
+        my_benchmark_session,
+        "sort_shuffled_sequence",
+        fplus::sort(shuffled_numbers);
+    );
+    // Verify that the sort has worked
+    assert(sorted_numbers == ascending_numbers);
 
-    // Convert gem_elements.size() into a std::string.
-    return show(size_of_cont(gem_elements));
+    // In this toy example, we will compare the performance 
+    // of sorting a shuffled sequence versus sorting a reversed sequence
+    
+    Ints descending_numbers = fplus::reverse(ascending_numbers); // this call is not benchmarked
+
+    // here we benchmark the call to fplus::sort(descending_numbers)
+    const auto sorted_numbers2 = benchmark_expression(
+        my_benchmark_session,
+        "sort_reverse_sequence",
+        fplus::sort(descending_numbers);
+    );
+    // Verify that the sort has worked
+    assert(sorted_numbers2 == ascending_numbers);
+
+    return 1;
 }
 
-
-TEST_CASE("my_benchmark")
+TEST_CASE("benchmark_example")
 {
-    std::string input("Lorem ipsum\ndolor sit amet,\nconsectetur,\nadipisci velit");
-    fplus::run_n_times(1000, [&]() { gemstones_bench(input); });
+    // Example 4 : benchmark by replacing a function
+    // We also want to benchmark the "benchmark_example" in its entirety
+    auto benchmark_example_bench = make_benchmark_function(
+        my_benchmark_session, 
+        "benchmark_example", 
+        benchmark_example);
 
-    // Will output something like
-    // Function   |Nb calls|Total time|Av. time|Deviation|
-    // -----------+--------+----------+--------+---------+
-    // transform  |    1000|   4.828ms| 4.828ns|  2.473ns|
-    // split_lines|    1000|   3.795ms| 3.795ns|  2.729ns|
+    // For the sake of this test, we will run the benchmarked function several times
+    fplus::run_n_times(10, [&]() { benchmark_example_bench(); });
+
     std::cout << fplus::show(my_benchmark_session.report());
+    // Will output something like
+    // Function              |Nb calls|Total time|Av. time   |Deviation |
+    // ----------------------+--------+----------+-----------+----------+
+    // benchmark_example     |      10| 136.494ms|13649.412ns|1191.401ns|
+    // sort_shuffled_sequence|      10|  66.698ms| 6669.850ns| 339.859ns|
+    // shuffle               |      10|  62.881ms| 6288.136ns| 754.399ns|
+    // sort_reverse_sequence |      10|   3.079ms|  307.911ns|  71.570ns|
+    // numbers               |      10|   2.364ms|  236.398ns| 134.988ns|
+
+    // Interestingly enough, we see that sort has a very good performance
+    // on reversed sequences. It proves that sort is not just based on qsort
+    // (qsort performs badly with reversed sequences)
 }
+
