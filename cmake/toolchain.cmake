1717cdfc8122cf1bf5af6d6c562a1f205d8dc8e5
Pascal Thomet <pthomet@gmail.com>
2018-10-15 15:29:55 +0200

    clang 7 : disable -Wreturn-std-move
    -Wreturn-std-move was introduced by clang 7:
    It causes an error at container_common.hpp: 338
    (sometimes std:move is required, sometimes not, depending on
    the template type)

--- cmake/toolchain.cmake
+++ cmake/toolchain.cmake
@@ -14,3 +14,15 @@ set(COMPILE_OPTIONS -Wall
 set(CMAKE_CXX_STANDARD 14)
 set(CMAKE_CXX_STANDARD_REQUIRED ON)
 set(CMAKE_CXX_EXTENSIONS OFF)
+
+
+####### Specific compiler adjustments
+include(CheckCXXCompilerFlag)
+
+# -Wreturn-std-move was introduced by clang 7:
+# It causes an error at container_common.hpp: 338
+# (sometimes std:move is required, sometimes not, depending on the template type)
+check_cxx_compiler_flag(-Wreturn-std-move compiler-has-warning-return-std-move)
+if (compiler-has-warning-return-std-move)
+  list(APPEND COMPILE_OPTIONS -Wno-return-std-move)
+endif()
