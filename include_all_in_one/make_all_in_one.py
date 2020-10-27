f29791d645e937bcbf00e86cc1e994cd6fd5d81a
Pascal Thomet <pthomet@gmail.com>
2020-10-27 23:57:32 +0100

    make_all_in_one: handle python2 and python3 file.open differences

--- include_all_in_one/make_all_in_one.py
+++ include_all_in_one/make_all_in_one.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 
 import os
+from sys import version_info
+
 THIS_DIR = os.path.dirname(os.path.realpath(__file__))
 FPLUS_INCLUDE_DIR = os.path.realpath(THIS_DIR + "/../include/fplus")
 
@@ -27,19 +29,29 @@ def parse_one_file(filename):
     ALLREADY_INCLUDED_FILES.append(filename)
 
     parsed_result = "\n//\n" + "// " + filename + "\n//\n\n"
-    with open(FPLUS_INCLUDE_DIR + "/" + filename, "r", encoding='utf-8', errors='ignore') as f:
-        lines = f.readlines()
-        for code_line in lines:
-            if is_fplus_include_line(code_line):
-                new_file = extract_fplus_include_file(code_line)
-                parsed_result = parsed_result + parse_one_file(new_file)
-            else:
-                if not "#pragma once" in code_line:
-                    parsed_result = parsed_result + code_line
+    
+    if version_info[0] >= 3:
+        f = open(FPLUS_INCLUDE_DIR + "/" + filename, "r", encoding='utf-8', errors='ignore')
+    else:
+        f = open(FPLUS_INCLUDE_DIR + "/" + filename, "r")
+    lines = f.readlines()
+    for code_line in lines:
+        if is_fplus_include_line(code_line):
+            new_file = extract_fplus_include_file(code_line)
+            parsed_result = parsed_result + parse_one_file(new_file)
+        else:
+            if not "#pragma once" in code_line:
+                parsed_result = parsed_result + code_line
+    f.close()
     return parsed_result
+
 
 print("-- Generating include_all_in_one")
 content = parse_one_file("fplus.hpp")
-with open(THIS_DIR + "/include/fplus/fplus.hpp", "w", encoding='utf-8', errors='ignore') as f:
-    f.write(content)
+if version_info[0] >= 3:
+    f = open(THIS_DIR + "/include/fplus/fplus.hpp", "w", encoding='utf-8', errors='ignore')
+else:
+    f = open(THIS_DIR + "/include/fplus/fplus.hpp", "w")
+f.write(content)
+f.close()
 
