ff79665cca5c8b00bc60338abb396f8935b466a3
offa <bm-dev@yandex.com>
2020-12-19 18:11:07 +0100

    Use locked version of doctest.

--- script/ci_setup_dependencies.sh
+++ script/ci_setup_dependencies.sh
@@ -2,7 +2,7 @@
 
 
 ## Install doctest
-git clone --depth=1 https://github.com/onqtam/doctest
+git clone --depth=1 --branch=2.3.4 https://github.com/onqtam/doctest
 cd doctest && mkdir -p build && cd build
 cmake .. -DDOCTEST_WITH_TESTS=OFF -DDOCTEST_WITH_MAIN_IN_STATIC_LIB=OFF
 make -j
