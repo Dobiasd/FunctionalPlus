9e63f4fceee9af0ad5165ff383f689c45f9ffedd
Dobiasd <editgym@gmail.com>
2020-12-25 17:24:25 +0100

    Remove Conan-related things from repo

--- test_package/test_package.cpp
+++
@@ -1,9 +0,0 @@
-#include <fplus/fplus.hpp>
-#include <iostream>
-
-int main()
-{
-    std::list<std::string> things = {"same old", "same old"};
-    if (fplus::all_the_same(things))
-        std::cout << "All things being equal." << std::endl;
-}
