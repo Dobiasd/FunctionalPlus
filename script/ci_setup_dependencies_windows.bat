75f69609cdddfb96900404930c8e8d7aaa95644e
Dobiasd <editgym@gmail.com>
2020-12-29 10:34:01 +0100

    Move windows commands into batch-script files

---
+++ script/ci_setup_dependencies_windows.bat
@@ -0,0 +1,4 @@
+git clone --depth=1 --branch=2.4.3 https://github.com/onqtam/doctest
+cd doctest && mkdir -p build && cd build
+cmake .. -DDOCTEST_WITH_TESTS=OFF -DDOCTEST_WITH_MAIN_IN_STATIC_LIB=OFF
+cmake --build . --config Release --target INSTALL
