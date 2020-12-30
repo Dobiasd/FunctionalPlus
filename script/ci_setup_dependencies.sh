863ca05d1e33c9d5988a424e6fa1acd1fef62e8f
Dobiasd <editgym@gmail.com>
2020-12-30 16:33:19 +0100

    Fix env var name

--- script/ci_setup_dependencies.sh
+++ script/ci_setup_dependencies.sh
@@ -6,8 +6,8 @@ git clone --depth=1 --branch=2.4.3 https://github.com/onqtam/doctest
 cd doctest && mkdir -p build && cd build
 cmake .. -DDOCTEST_WITH_TESTS=OFF -DDOCTEST_WITH_MAIN_IN_STATIC_LIB=OFF
 
-BUILDUSINGCMAKE=${BUILDUSINGCMAKE:-false}
-if [ $BUILDUSINGCMAKE = true ]
+BUILD_USING_CMAKE=${BUILD_USING_CMAKE:-false}
+if [ $BUILD_USING_CMAKE = true ]
 then
     cmake --build . --config Release --target INSTALL
 else
