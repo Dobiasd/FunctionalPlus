aa673abbd4d0e0a0cde39af262ff9183b49544d3
Dobiasd <editgym@gmail.com>
2020-12-31 08:54:40 +0100

    Use target install instead of INSTALL

--- script/ci_setup_dependencies.sh
+++ script/ci_setup_dependencies.sh
@@ -5,10 +5,9 @@
 git clone --depth=1 --branch=2.4.3 https://github.com/onqtam/doctest
 cd doctest && mkdir -p build && cd build
 cmake .. -DDOCTEST_WITH_TESTS=OFF -DDOCTEST_WITH_MAIN_IN_STATIC_LIB=OFF
-BUILD_DOCTEST_USING_CMAKE=${BUILD_DOCTEST_USING_CMAKE:-false}
 if [ $BUILD_DOCTEST_USING_CMAKE = true ]
 then
-    cmake --build . --config Release --target INSTALL
+    cmake --build . --config Release --target install
 else
     make -j
     make install
