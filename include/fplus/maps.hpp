aad1df0627065c7c340af4eafad4d0db6861cad7
Unix&Me <unixnme@gmail.com>
2021-03-15 10:26:44 -0400

    rename

--- include/fplus/maps.hpp
+++ include/fplus/maps.hpp
@@ -116,14 +116,14 @@ MapType map_union(const MapType& dict1, const MapType& dict2)
     return map_union_with(get_first, dict1, dict2);
 }
 
-// API search type: name_not_yet_decided : Map key [val] -> [(key, val)]
+// API search type: map_grouped_to_pairs : Map key [val] -> [(key, val)]
 // fwd bind count: 0
 // Convert a dictionary with sequence values to a list of key-value pairs.
 // Inverse operation of pairs_to_map_grouped.
-// name_not_yet_decided({"a": [1, 2, 4], "b": [6]})
+// map_grouped_to_pairs({"a": [1, 2, 4], "b": [6]})
 //     -> [("a", 1), ("a", 2), ("a", 4), ("b", 6)]
 template<typename MapType>
-auto name_not_yet_decided(const MapType &dict) {
+auto map_grouped_to_pairs(const MapType &dict) {
     using key_type = typename MapType::key_type;
     using group_type = typename MapType::mapped_type;
 
