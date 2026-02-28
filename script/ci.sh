90982b3b69e3f2686f07b0071ea288aa672dbaba
offa <8887756+offa@users.noreply.github.com>
2026-02-28 20:52:33 +0100

    Fix doctest CMake compatibility

--- script/ci.sh
+++ script/ci.sh
@@ -28,7 +28,7 @@ fi
 # Function to install doctest
 _install_doctest() {
     cd "$REPO_DIR"
-    git clone --depth=1 --branch=v2.4.11 https://github.com/doctest/doctest
+    git clone --depth=1 --branch=v2.4.12 https://github.com/doctest/doctest
     cd doctest && mkdir -p build && cd build
     cmake .. -DDOCTEST_WITH_TESTS=OFF -DDOCTEST_WITH_MAIN_IN_STATIC_LIB=OFF -DCMAKE_INSTALL_PREFIX=$INSTALL_PREFIX
     cmake --build . --config Release --target install
